aac225dbc81364d798ca826dd17e89d5762493a5
s.dixon <s.dixon@xangars.com>
2021-02-15 12:17:38 +0530

    File Upload changes

--- Pre-claim-update/src/main/java/com/example/Preclaimupdate/common/Config.java
+++ Pre-claim-update/src/main/java/com/example/Preclaimupdate/common/Config.java
@@ -2,6 +2,6 @@ package com.example.Preclaimupdate.common;
 
 public interface Config {
 
-	String uploadDirectory = "C:\\Pre-Claim Investigation\\uploads";
-	String uploadURL = "C:\\Pre-Claim Investigation\\uploads";
+	String uploadDirectory = "C:\\Pre-Claim Investigation\\uploads\\";
+	String uploadURL = "C:\\Pre-Claim Investigation\\uploads\\";
 }
