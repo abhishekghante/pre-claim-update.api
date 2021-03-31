d77171bf0ba91ea0c5777f1871c43885d565e2b2
s.dixon <s.dixon@xangars.com>
2021-03-31 21:25:53 +0530

    Removed warning message

--- Pre-claim-update/src/main/java/com/example/Preclaimupdate/controller/Repository/Case_movementRepository.java
+++ Pre-claim-update/src/main/java/com/example/Preclaimupdate/controller/Repository/Case_movementRepository.java
@@ -1,13 +1,10 @@
 package com.example.Preclaimupdate.controller.Repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.Preclaimupdate.entity.Case_lists;
 import com.example.Preclaimupdate.entity.Case_movement;
 
 @Repository
